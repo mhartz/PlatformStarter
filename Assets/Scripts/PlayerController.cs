6f25b0334ff05cf99e1cbb431509711ee1259b1b
mhartz <mshartz5@gmail.com>
2017-12-12 23:09:48 -0500

    some more stuff changed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,97 +1,101 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-    public float MoveSpeed;
-    public float JumpSpeed;
-    public bool AllowDoubleJump;
+namespace Assets.Scripts
+{
+    public class PlayerController : MonoBehaviour {
+        public float MaxMoveSpeed;
+        public float MoveForce;
+        public float JumpSpeed;
+        public bool AllowDoubleJump;
+        public bool AllowRun;
 
-    private Rigidbody2D rb;
-    private bool IsGrounded;
-    private int JumpCount = 0;
+        private Vector3 _playerControls;
+        private Rigidbody2D _rb;
+        private bool _isGrounded;
+        private int _jumpCount = 0;
+        private float _resetMoveSpeed;
+        private float _runSpeed;
 
-	void Start () {
-        rb = GetComponent<Rigidbody2D>();
-	}
+        void Start () {
+            _rb = GetComponent<Rigidbody2D>();
+            _resetMoveSpeed = MaxMoveSpeed;
+            _runSpeed = MaxMoveSpeed * 2f;
+        }
 	
-	void Update () {
-		MovePlayer();
-
-	    if (AllowDoubleJump)
-	    {
-	        PlayerDoubleJump();
-	    }
-	    else
-	    {
-	        PlayerJump();
-        }
-	}
-
-    private void MovePlayer()
-    {
-        if (Input.GetAxisRaw("Horizontal") > 0f)
-        {
-            rb.velocity = new Vector3(MoveSpeed, rb.velocity.y, 0f);
-            transform.localScale = new Vector3(1f, 1f, 1f);
-        }
-        else if (Input.GetAxisRaw("Horizontal") < 0f)
-        {
-            rb.velocity = new Vector3(-MoveSpeed, rb.velocity.y, 0f);
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-        }
-        else
-        {
-            rb.velocity = new Vector3(0f, rb.velocity.y, 0f);
-        }
-    }
-
-    private void PlayerJump()
-    {
-        if (Input.GetButtonDown("Jump") && IsGrounded)
-        {
-            rb.velocity = new Vector3(rb.velocity.x, JumpSpeed, 0f);
-        }
-    }
-
-    private void PlayerDoubleJump()
-    {
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (JumpCount < 2)
-            {
-                rb.velocity = new Vector3(rb.velocity.x, JumpSpeed, 0f);
-                JumpCount++;
-            }                
-        }
-    }
-
-    private void Instakill()
-    {
-        //Todo : Need to implement a respawn based on the Game Manager
-
-        transform.position = new Vector3(0, 0, 0);
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
-            IsGrounded = true;
+        void Update () {
+            _playerControls = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
 
             if (AllowDoubleJump)
-                JumpCount = 0;
+                PlayerDoubleJump();
+            else
+                PlayerJump();
         }
 
-        if (collision.gameObject.tag == "Instakill")
-            Instakill();
-    }
-
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
+        void FixedUpdate()
         {
-            IsGrounded = false;
+            MovePlayer();
+        }
+
+        private void MovePlayer()
+        {
+            //Running when shift it held
+            if (AllowRun && Input.GetButton("Run"))
+                MaxMoveSpeed = _runSpeed;
+            else
+                MaxMoveSpeed = _resetMoveSpeed;
+
+            _rb.velocity = Vector3.ClampMagnitude(_rb.velocity, MaxMoveSpeed);
+            _rb.AddForce(_playerControls.normalized * MoveForce);
+            Debug.Log(_playerControls.normalized * MoveForce);
+        }
+
+        private void PlayerJump()
+        {
+            if (Input.GetButtonDown("Jump") && _isGrounded)
+            {
+                _rb.velocity = new Vector3(_rb.velocity.x, JumpSpeed, 0f);
+            }
+        }
+
+        private void PlayerDoubleJump()
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                if (_jumpCount < 2)
+                {
+                    _rb.velocity = new Vector3(_rb.velocity.x, JumpSpeed, 0f);
+                    _jumpCount++;
+                }                
+            }
+        }
+
+        private void Instakill()
+        {
+            //Todo : Need to implement a respawn based on the Game Manager
+
+            transform.position = new Vector3(0, 0, 0);
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "Ground")
+            {
+                _isGrounded = true;
+
+                if (AllowDoubleJump)
+                    _jumpCount = 0;
+            }
+
+            if (collision.gameObject.tag == "Instakill")
+                Instakill();
+        }
+
+        void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "Ground")
+            {
+                _isGrounded = false;
+            }
         }
     }
 }
